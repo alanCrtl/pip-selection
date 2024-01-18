41ead171f1d4cb59a8a6a1ec2eaea992beeda8c1
alanCrtl <alan.courtel1999@gmail.com>
2024-01-18 14:02:44 +0100

    better sized picture and clientX/Y changed to pageX/Y

--- README.md
+++ README.md
@@ -2,14 +2,10 @@
 
 Work in progress !
 
-## Current state
-
-![Alt Text](usage.gif)
-
 ## ressources
 
-This allows you to populate PiP with pretty much anything: [document picture-in-picture](https://wicg.github.io/document-picture-in-picture/)  
-To get screenshot from a rectangle selection: [html2canvas docs](https://html2canvas.hertzen.com/documentation)
+This allows you to populate PiP with pretty much anything: --- [document picture-in-picture](https://wicg.github.io/document-picture-in-picture/)  
+To get screenshot from a rectangle selection --- [html2canvas docs](https://html2canvas.hertzen.com)
 
 ## test it for yourself
 
@@ -21,13 +17,16 @@ draw a rectangle and see result
 
 ## explanation
 
-Works in essence but the picture created is actually
-not at all what you select, it's missing elements 
-that don't display and it's not properly framed so 
-still some work...
+Works in essence, the limitation for now is that html2canvas  
+doesn't wait for images or loaded content when taking screenshot  
+so it is useful for text atleast. Also it doesn't work on every  
+site, test it on wikipedia for example not on twitch or youtube.
 
 ## TODO:
 
-maybe find better way to take screenshot from selection  
-wrong dimension of image  
-pip window not well sized for image  
+- a bit slow
+- findout why it doesn't work on some sites (yt, twitch)
+- add shortcut for taking screenshot, and options to setup the keybind yourself
+- add css to popup.html
+- remove red rectangle and opactiy from overlay in the actual screenshot
+- maybe find better way to take screenshot from selection  
--- content.js
+++ content.js
@@ -33,22 +33,53 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 });
 
 // =========== functions =========== 
+// ===== Mouse move events =====
 function handleMouseDown(event) {
     event.preventDefault(); // Prevent text selection
     isSelecting = true;
-    startX = event.clientX;
-    startY = event.clientY;
+    startX = event.pageX;
+    startY = event.pageY;
 }
 
 function handleMouseMove(event) {
     if (isSelecting) {
-        endX = event.clientX;
-        endY = event.clientY;
+        endX = event.pageX;
+        endY = event.pageY;
 
         drawSelectionRectangle(startX, startY, endX, endY);
     }
 }
 
+function handleMouseUp() {
+    if (isSelecting) {
+        isSelecting = false;
+
+        // Capture the selected area as an image
+        console.log(`coords: sx ex sy ey ${[startX, endX, startY, endY]}`)
+        captureSelectedArea()
+            .then(selectedImage => {
+                // Display the image in a PiP window
+                showImageInPiP(selectedImage);
+            })
+            .catch(error => {
+                console.error('Error capturing the selected area:', error);
+            })
+
+            // Clear the selection box
+            clearSelectionBox();
+            // Remove event listeners
+            document.removeEventListener('mousedown', handleMouseDown);
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+            // remove overlay
+            if (overlay) {
+                overlay.remove();
+                overlay = null;
+            }
+    }
+}
+
+// ===== Rectangle drawing and clearing functions =====
 function drawSelectionRectangle(startX, startY, endX, endY) {
     // Remove any existing rectangles
     const existingRect = document.getElementById('rectangle-selector');
@@ -78,58 +109,31 @@ function drawSelectionRectangle(startX, startY, endX, endY) {
     document.body.appendChild(rectangle);
 }
 
-function handleMouseUp() {
-    if (isSelecting) {
-        isSelecting = false;
-        // Capture the selected area as an image
-        console.log(`coords: sx ex sy ey ${[startX, endX, startY, endY]}`)
-        captureSelectedArea()
-            .then(selectedImage => {
-                // Display the image in a PiP window
-                showImageInPiP(selectedImage);
-            })
-            .catch(error => {
-                console.error('Error capturing the selected area:', error);
-            })
-
-            // Clear the selection box
-            clearSelectionBox();
-            // Remove event listeners
-            document.removeEventListener('mousedown', handleMouseDown);
-            document.removeEventListener('mousemove', handleMouseMove);
-            document.removeEventListener('mouseup', handleMouseUp);
-            // remove overlay
-            if (overlay) {
-                overlay.remove();
-                overlay = null;
-            }
-    }
-}
-
 function clearSelectionBox() {
     const rectangleSelector = document.getElementById('rectangle-selector');
     rectangleSelector.style.width = '0';
     rectangleSelector.style.height = '0';
 
-    // Reset cursor position
     startX = 0;
     startY = 0;
     endX = 0;
     endY = 0;
 }
 
+// ======== Screenshot functions ==========
 function captureSelectedArea() {
     console.log(`Width; height; x; y ${[Math.abs(endX - startX), Math.abs(endY - startY), Math.min(startX, endX), Math.min(startY, endY)]}`)
     return new Promise((resolve, reject) => {
         html2canvas(document.documentElement, {
+            // windowHeight: Math.abs(endY - startY),
+            // windowWidth: Math.abs(endX - startX),
+
             width: Math.abs(endX - startX),
             height: Math.abs(endY - startY),
-            windowWidth: Math.abs(endX - startX),
-            windowHeight: Math.abs(endY - startY),
             x: Math.min(startX, endX),
             y: Math.min(startY, endY),
-            scrollX: 0,
-            scrollY: 0,
+            // scrollX: 0,
+            // scrollY: 0,
         }).then(canvas => {
               resolve(canvas.toDataURL('image/png'));
             })
@@ -142,15 +146,14 @@ function captureSelectedArea() {
 function showImageInPiP(imageDataURL) {
     const img = document.createElement('img');
     img.src = imageDataURL;
-    console.log(`image dimensions ${[img.width, img.height]}`)
 
     // Listen for the 'load' event before attempting to get the dimensions
     img.onload = function() {
         console.log(`Image dimensions: [${img.width}, ${img.height}]`);
-
+        extend_dimensions_px = 60
         const pipOptions = {
-            width: img.width,
-            height: img.height,
+            width: img.width + extend_dimensions_px,
+            height: img.height + extend_dimensions_px,
         };
 
         documentPictureInPicture.requestWindow(pipOptions).then(pipWin => {
